61f90d2039355f95311ff5866997015e0130684c
Rijenth <a_rijenth@laposte.net>
2023-09-12 11:24:33 +0200

    update test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -10,44 +10,49 @@ describe("Gilded Rose", function () {
         });
     });
 
-    it("Gilded Rose updateQuality with one item", function () {
+    it("Gilded Rose updateQuality with one item with sellIn = 0", function () {
         runGoldenMaster(async () => {
-            const gildedRose = new GildedRose([new Item("Cheese", 0, 0)]);
+            const gildedRose = new GildedRose([new Item("Cheese", 0, 2)]);
 
             gildedRose.updateQuality();
         });
     });
 
-    it("Gilded Rose updateQuality with one Aged Brie", function () {
+    it("Gilded Rose updateQuality with one item with quality = 0", function () {
         runGoldenMaster(async () => {
-            const gildedRose = new GildedRose([new Item("Aged Brie", 0, 0)]);
+            const gildedRose = new GildedRose([new Item("Cheese", 2, 0)]);
 
             gildedRose.updateQuality();
         });
     });
 
-    it("Gilded Rose updateQuality with one Backstage passes to a TAFKAL80ETC concert", function () {
+    it("Gilded Rose updateQuality with one Aged Brie with sellIn = 0", function () {
         runGoldenMaster(async () => {
-            const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 0)]);
+            const gildedRose = new GildedRose([new Item("Aged Brie", 0, 10)]);
 
             gildedRose.updateQuality();
         });
     });
 
-    it("Gilded Rose updateQuality with one Sulfuras, Hand of Ragnaros", function () {
+    it("Gilded Rose updateQuality with one Backstage passes to a TAFKAL80ETC concert with sellIn = 0", function () {
         runGoldenMaster(async () => {
-            const gildedRose = new GildedRose([new Item("Sulfuras, Hand of Ragnaros", 0, 0)]);
+            const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)]);
 
             gildedRose.updateQuality();
         });
     });
 
-
-    it("Gilded Rose updateQuality with one item and quality > 0", function () {
+    it("Gilded Rose updateQuality with one Sulfuras, Hand of Ragnaros with sellIn = 0", function () {
         runGoldenMaster(async () => {
-            const gildedRose = new GildedRose([
-                new Item("Cheese", 0, 2),
-            ]);
+            const gildedRose = new GildedRose([new Item("Sulfuras, Hand of Ragnaros", 0, 50)]);
+
+            gildedRose.updateQuality();
+        });
+    });
+
+    it("Gilded Rose updateQuality with one Sulfuras, Hand of Ragnaros with sellIn > 0", function () {
+        runGoldenMaster(async () => {
+            const gildedRose = new GildedRose([new Item("Sulfuras, Hand of Ragnaros", 2, 50)]);
 
             gildedRose.updateQuality();
         });
